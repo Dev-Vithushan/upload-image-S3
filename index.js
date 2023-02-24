08823bcc2e1b4916f419298dcdb6836db4e410b1
Vithushan Logeswaran <2018csc042@univ.jfn.ac.lk>
2023-02-24 11:43:11 +0530

    added the multifiles code

--- index.js
+++ index.js
@@ -5,13 +5,25 @@ const app = express()
 const upload = multer({dest:"upload/"})
 
 //single file upload
-// app.post("/upload", upload.single("file"), (req,res)=>{
-//     res.json({status:"success"})
-// })
+app.post("/upload", upload.single("file"), (req,res)=>{
+    res.json({status:"success"})
+})
 
 //Multiple file upload
 app.post("/upload", upload.array("file"), (req,res)=>{
     res.json({status:"success"})
 })
+
+
+//Multiple file upload with different name
+const multiUpload = upload.fields([
+    {name:"avatar", maxCount:1},
+    {name:"resume", maxCount:1}
+])
+
+app.post("/upload", multiUpload, (req,res)=>{
+    res.json({status:"success"})
+})
+
 
 app.listen(3000, ()=>{ console.log("Connected")})
