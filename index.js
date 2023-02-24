b9f52b3ec2c7dd716f65b85d158b9084c1aa3556
Vithushan Logeswaran <2018csc042@univ.jfn.ac.lk>
2023-02-24 11:29:01 +0530

    Adding the file code added

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express=  require("express")
-
+const multer = require("multer")
 const app = express()
 
-app.post("/upload", (res,req)=>{
+const upload = multer({dest:"upload/"})
+
+app.post("/upload", upload.single("file"), (req,res)=>{
     res.json({status:"success"})
 })
+
+
+app.listen(3000, ()=>{ console.log("Connected")})
